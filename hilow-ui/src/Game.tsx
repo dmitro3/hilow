8ec0f537fb696c621bb3e31a045ad84727ba62c5
Alex Mathew <alexmathew003@gmail.com>
2022-05-29 01:34:05 +0530

    Try out a contract interaction

--- hilow-ui/src/Game.tsx
+++ hilow-ui/src/Game.tsx
@@ -1,20 +1,32 @@
 import React, { useEffect } from "react";
-import { VStack, Text, HStack } from "@chakra-ui/react";
-import { useAccount } from "wagmi";
+import { VStack, Text, HStack, Button } from "@chakra-ui/react";
+import { useAccount, useContractWrite, useContractRead } from "wagmi";
 import { useFlowUpdate } from "./context/FlowContext";
 import { FlowStates } from "./constants";
 import Header from "./Header";
+import HilowABI from "./abi/Hilow.json";
 
 interface GameProps {}
 
 const Game: React.FC<GameProps> = () => {
   const { data: accountData } = useAccount();
   const updateFlow = useFlowUpdate();
+  const contractConfig = {
+    addressOrName: "0x4C2b3104f55735Dd5A4a57D0d0b36cBe165c47BA",
+    contractInterface: HilowABI.abi,
+  };
+  // const contractRead = useContractRead(
+  //   contractConfig,
+  //   'viewCards',
+  // )
+  const { write: tipDealer } = useContractWrite(contractConfig, "tip");
 
   useEffect(() => {
     if (accountData) updateFlow(FlowStates.GAME);
     else updateFlow(FlowStates.CONNECT);
   }, [accountData]);
+
+  // useEffect(() => {console.log(contractRead)}, [contractRead])
 
   return (
     <>
@@ -29,6 +41,13 @@ const Game: React.FC<GameProps> = () => {
           >
             Game!
           </Text>
+          <Button
+            variant="outline"
+            _hover={{ bg: "black", borderColor: "black", color: "white" }}
+            onClick={() => tipDealer({ overrides: { value: 100000000000000 } })}
+          >
+            Tip
+          </Button>
         </HStack>
       </VStack>
     </>
